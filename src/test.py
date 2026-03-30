f128074c4be61e4240a9a51d5eb7350f6eb48069
vasikandos <vasikandos@gmail.com>
2026-03-30 10:53:28 +0200

    Update test.py
    Importoval jsem knihovnu math přidal jsem víc testů na float, udělal jsem testy na odmocniny. Všechny testy by měli být hotové a už bych měl jenom upravovat a kontrolovat jestli testy jsou všechny validní

--- src/test.py
+++ src/test.py
@@ -1,5 +1,5 @@
 import calc 
-
+import math
 
 ###########################
 # Testy add 
@@ -15,11 +15,14 @@ def test_calc_add_positive():
 def test_calc_add_negative():
     assert calc.add(-2, -3) == -5
     assert calc.add(-4, -2) == -6
+    assert calc.add(-2.5, -3.5) == -6.0
 
 # Testy na přidání smíšených čísel
 def test_calc_add_mixed():
     assert calc.add(2, -3) == -1
     assert calc.add(-2, 3) == 1
+    assert calc.add(2.5, -3.5) == -1.0
+    assert calc.add(-2.5, 3.5) == 1.0
 
 # Testy na přidání nuly    
 def test_calc_add_zero():
@@ -28,6 +31,8 @@ def test_calc_add_zero():
     assert calc.add(5, 0) == 5
     assert calc.add(0, -5) == -5
     assert calc.add(-5, 0) == -5
+    assert calc.add(2.5, 0) == 2.5
+    assert calc.add(0, 2.5) == 2.5
 
 
 
@@ -41,17 +46,23 @@ def test_calc_add_zero():
 # Testy na odčítaní kladných čísel
 def test_calc_subtract_positive():
     assert calc.subtract(5, 2) == 3
+    assert calc.subtract(2, 5) == -3
+    assert calc.subtract(5.5, 2.5) == 3.0
+    assert calc.subtract(2.5, 5.5) == -3.0
 
 # Testy na odčítaní záporných čísel
 def test_calc_subtract_negative():
     assert calc.subtract(-5, -2) == -3
+    assert calc.subtract(-2, -5) == 3
+    assert calc.subtract(-5.5, -2.5) == -3.0
+    assert calc.subtract(-2.5, -5.5) == 3.0
 
 # Testy na odčítaní smíšených čísel
 def test_calc_subtract_mixed():
     assert calc.subtract(2, -3) == 5
     assert calc.subtract(-2, 3) == -5
-    assert calc.subtract(2, 3) == -1
-    assert calc.subtract(-2, -3) == 1
+    assert calc.subtract(2.5, -3.5) == 6.0
+    assert calc.subtract(-2.5, 3.5) == -6.0
 
 # Testy na odčítaní nuly
 def test_calc_subtract_zero():
@@ -60,7 +71,8 @@ def test_calc_subtract_zero():
     assert calc.subtract(5, 0) == 5
     assert calc.subtract(0, -5) == 5
     assert calc.subtract(-5, 0) == -5
-
+    assert calc.subtract(0, 2.5) == -2.5
+    assert calc.subtract(2.5, 0) == 2.5
 
 
 
@@ -73,16 +85,22 @@ def test_calc_subtract_zero():
 def test_calc_multiply_positive():
     assert calc.multiply(2, 3) == 6
     assert calc.multiply(3, 2) == 6
+    assert calc.multiply(2.5, 3.5) == 8.75
+    assert calc.multiply(3.5, 2.5) == 8.75
 
 # Testy na násobení záporných čísel
 def test_calc_multiply_negative():
     assert calc.multiply(-2, -3) == 6
     assert calc.multiply(-3, -2) == 6
+    assert calc.multiply(-2.5, -3.5) == 8.75
+    assert calc.multiply(-3.5, -2.5) == 8.75
    
 # Testy na násobení smíšených čísel
 def test_calc_multiply_mixed():
     assert calc.multiply(2, -3) == -6
     assert calc.multiply(-2, 3) == -6
+    assert calc.multiply(2.5, -3.5) == -8.75
+    assert calc.multiply(-2.5, 3.5) == -8.75
 
 # Testy na násobení nuly
 def test_calc_multiply_zero():
@@ -91,6 +109,8 @@ def test_calc_multiply_zero():
     assert calc.multiply(5, 0) == 0
     assert calc.multiply(0, -5) == 0
     assert calc.multiply(-5, 0) == 0
+    assert calc.multiply(0, 2.5) == 0
+    assert calc.multiply(2.5, 0) == 0
 
 
 
@@ -105,18 +125,24 @@ def test_calc_divide_positive():
     assert calc.divide(10, 2) == 5
     assert calc.divide(10, 5) == 2
     assert calc.divide(2, 10) == 0.2
+    assert calc.divide(5.5, 2.5) == 2.2
+    assert math.isclose(calc.divide(2.5, 5.5), 0.45454545454545453)
 
 # Testy na dělení záporných čísel
 def test_calc_divide_negative():
     assert calc.divide(-10, -2) == 5
     assert calc.divide(-10, -5) == 2
     assert calc.divide(-2, -10) == 0.2
+    assert calc.divide(-5.5, -2.5) == 2.2
+    assert math.isclose(calc.divide(-2.5, -5.5), 0.45454545454545453)
 
 # Testy na dělení smíšených čísel
 def test_calc_divide_mixed():
     assert calc.divide(10, -2) == -5
     assert calc.divide(-10, 2) == -5
     assert calc.divide(2, -10) == -0.2
+    assert calc.divide(-5.5, 2.5) == -2.2
+    assert math.isclose(calc.divide(2.5, -5.5), -0.45454545454545453)
 
 
 # Testy na dělení nuly
@@ -126,7 +152,10 @@ def test_calc_divide_zero():
     assert calc.divide(0, 0) == "Error: Division by zero"
     assert calc.divide(5, 0) == "Error: Division by zero"
     assert calc.divide(-5, 0) == "Error: Division by zero"
-
+    assert calc.divide(0, 2.5) == 0
+    assert calc.divide(0, -2.5) == 0
+    assert calc.divide(2.5, 0) == "Error: Division by zero"
+    assert calc.divide(-2.5, 0) == "Error: Division by zero"
 
 
 ###########################
@@ -139,17 +168,28 @@ def test_calc_power_positive():
     assert calc.power(2, 3) == 8
     assert calc.power(2, 2) == 4
     assert calc.power(2, 1) == 2
+    assert calc.power(2.5, 1) == 2.5
+    assert calc.power(2.5, 2) == 6.25
+    assert math.isclose(calc.power(2, 2.5), 5.656854249492381)
 
 # Testy na mocniny záporných čísel
 def test_calc_power_negative():
     assert calc.power(-2, -1) == -0.5
     assert calc.power(-2, -2) == 0.25
+    assert calc.power(-2.5, -3) == -0.064
+    assert calc.power(-2.5, -2) == 0.16
+    assert math.isclose(calc.power(-2, 2.5), -5.656854249492381)
 
 # Testy na mocniny smíšených čísel
 def test_calc_power_mixed():
     assert calc.power(-2, 3) == -8
     assert calc.power(-2, 2) == 4
     assert calc.power(2, -1) == 0.5
+    assert calc.power(2, -2) == 0.25
+    assert calc.power(2.5, -2) == 0.16
+    assert calc.power(-2.5, 2) == 6.25
+    assert math.isclose(calc.power(-2, 2.5), -5.656854249492381)
+    assert math.isclose(calc.power(2, -2.5), 0.1767766952966369)
 
 # Testy na mocniny nuly
 def test_calc_power_zero():
@@ -158,6 +198,10 @@ def test_calc_power_zero():
     assert calc.power(0, -2) == "Error: 0 cannot be raised to a negative power"
     assert calc.power(2, 0) == 1
     assert calc.power(-2, 0) == 1
+    assert calc.power(0, 2.5) == 0
+    assert calc.power(0, -2.5) == "Error: 0 cannot be raised to a negative power"
+    assert calc.power(2.5, 0) == 1
+    assert calc.power(-2.5, 0) == 1
 
 
 
@@ -169,17 +213,48 @@ def test_calc_power_zero():
 
 # Testy na odmocniny kladných čísel
 def test_calc_sqrt_positive():
-    assert calc.sqrt(4) == 2
-
+    assert calc.sqrt(4, 2) == 2
+    assert calc.sqrt(9, 2) == 3
+    assert calc.sqrt(27, 3) == 3
+    assert calc.sqrt(16, 4) == 2
+    assert calc.sqrt(2.25, 2) == 1.5
+    assert calc.sqrt(1, 3) == 1
+    assert calc.sqrt(1, 2) == 1
+    assert calc.sqrt(2.25, 2) == 1.5
+    assert math.isclose(calc.sqrt(2, 2.5), 1.3195079107728942)
 
 # Testy na odmocniny záporných čísel
 def test_calc_sqrt_negative():
-    assert calc.sqrt(-2) == "Error: "
+    assert calc.sqrt(-2, -2) == "Error: Square root is not defined for negative numbers"
+    assert calc.sqrt(-8, -3) == "Error: Cube root is not defined for negative numbers"
+    assert calc.sqrt(-16, -4) == "Error: Fourth root is not defined for negative numbers"
+    assert calc.sqrt(-2.25, -2) == "Error: Square root is not defined for negative numbers"
+    assert calc.sqrt(-1, -3) == "Error: Cube root is not defined for negative numbers"
+
+# Testy na odmocniny smíšených čísel
+def test_calc_sqrt_mixed():
+    assert calc.sqrt(2, -2) == 0.25
+    assert calc.sqrt(-2, 2) == "Error: Square root is not defined for negative numbers"
+    assert calc.sqrt(8, -3) == 0.5
+    assert calc.sqrt(-8, 3) == "Error: Cube root is not defined for negative numbers"
+    assert math.isclose(calc.sqrt(2.25, -2), 0.6666666666666666)
+    assert calc.sqrt(-2.25, 2) == "Error: Square root is not defined for negative numbers"
+    assert math.isclose(calc.sqrt(2, 2.5), 1.3195079107728942)
+    assert calc.sqrt(-2, 2.5) == "Error: Square root is not defined for negative numbers"
 
 # Testy na odmocniny nuly
 def test_calc_sqrt_zero():
-    assert calc.sqrt(0) == 0
-
+    assert calc.sqrt(0, 2) == 0
+    assert calc.sqrt(0, 3) == 0
+    assert calc.sqrt(0, 4) == 0
+    assert calc.sqrt(0, 2.5) == 0
+    assert calc.sqrt(0, -2) == "Error: 0 cannot be raised to a negative power"
+    assert calc.sqrt(0, -3) == "Error: 0 cannot be raised to a negative power"
+    assert calc.sqrt(1, 0) == "Error: Root degree cannot be zero"
+    assert calc.sqrt(5, 0) == "Error: Root degree cannot be zero"
+    assert calc.sqrt(0, 0) == "Error: Root degree cannot be zero"
+    assert calc.sqrt(2.5, 0) == "Error: Root degree cannot be zero"
+     
 ###########################
  # Testy na faktoriál 
 
@@ -198,6 +273,7 @@ def test_calc_factorial_positive():
 def test_calc_factorial_negative():
     assert calc.factorial(-3) == "Error: Factorial is not defined for negative numbers" 
     assert calc.factorial(-1) == "Error: Factorial is not defined for negative numbers"
+    assert calc.factorial(-2.5) == "Error: Factorial is not defined for negative numbers"  
 
 # Testy na faktoriál nuly
 def test_calc_factorial_zero():
